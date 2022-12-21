64879633015330e50e1976868c79beb4964bd4d3
haozhenhua <1305510482@qq.com>
2022-12-21 23:49:08 +0900

    update

--- dags/build/update.sh
+++ dags/build/update.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+if [ -e tmp ]
+then
+rm -rf tmp
+mkdir tmp
+chmod 777 tmp
+echo '已删除并重新创建 tmp 目录'
+else
+mkdir tmp
+chmod 777 tmp
+echo 'tmp 目录不存在，已创建'
+fi
+
+echo start 
+pwd
+ls
+cp -r *.py tmp/
+ls tmp/
 for FILE in tmp/*.py
   do
     echo $FILE

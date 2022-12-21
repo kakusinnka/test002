eb1f04530afffceabe60f49b1d048168fc0a8ec4
haozhenhua <1305510482@qq.com>
2022-12-21 23:13:44 +0900

    update

---
+++ dags/build/modify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for FILE in tmp/*.py
+  do
+    echo $FILE
+    oldDagIdLine=$(grep "^def " $FILE)
+    echo $oldDagIdLine
+    oldDagIdStr=${oldDagIdLine%:*}
+    oldDagIdStr=${oldDagIdStr:4}
+    echo $oldDagIdStr
+    newDagIdStr=${oldDagIdStr%(*}"_sandbox()"
+    echo $newDagIdStr
+    sed -i "s/$oldDagIdStr/$newDagIdStr/" $FILE
+
+    line=$(sed -n '/default_args/=' $FILE)
+    ((line++))
+    echo $line
+    ownerStr="'owner': 'max',"
+    echo $ownerStr
+    sed -i "${line}i \    $ownerStr" $FILE
+done
